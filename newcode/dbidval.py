eb6dc897993508e4875d6707b754fcb679fda35b
Amnon Amir <amnonim@gmail.com>
2016-09-18 00:16:01 -0700

    ontology and client

--- newcode/dbidval.py
+++ newcode/dbidval.py
@@ -1,4 +1,5 @@
 from utils import debug
+import psycopg2
 
 
 def GetIdFromDescription(con,cur,table,description,noneok=False):
@@ -32,6 +33,82 @@ def GetIdFromDescription(con,cur,table,description,noneok=False):
 		cid=cur.fetchone()[0]
 		debug(2,"value %s found in table %s id %d" % (description,table,cid))
 		return cid
-	except:
-		debug(8,"error in GetIdFromValue")
+	except psycopg2.DatabaseError as e:
+		debug(8,"error %s in GetIdFromValue" % e)
 		return -2
+
+
+def AddItem(con,cur,table,description,allowreplicate=False,commit=True):
+	"""
+	Add a id,description to table and return the id.
+	If item already exists, behavior depends on allowreplicate:
+		False (default) - just return the id of the existing item
+		True - add a new item
+
+	input:
+	con,cur
+	table : str
+		Name of the table to search
+	description : str
+		the description to add
+	allowreplicate : bool (optional)
+		False (default) - just return the id of the existing item
+		True - add a new item
+	commit : bool (optional)
+		True (default) to commit, False to skip the commit
+
+	output:
+	err : str
+		Error message or empty string if ok
+	sid : int
+		the id of the added item
+	"""
+	try:
+		description=description.lower()
+		if not allowreplicate:
+			# search if exists
+			sid=GetIdFromDescription(con,cur,table,description)
+			if sid>=0:
+				debug(2,'AddItem - item %s already exists. id is %d' % (description,sid))
+				return '',sid
+		# should create new item
+		cur.execute('INSERT INTO %s (description) VALUES (%s) RETURNING id' % (table,'%s'),[description])
+		sid=cur.fetchone()[0]
+		debug(2,'AddItem - added new item %s. id is %d' % (description,sid))
+		if commit:
+			con.commit()
+		return '',sid
+	except psycopg2.DatabaseError as e:
+		debug(8,"error %s in AddItem" % e)
+		return 'Error %s in AddItem' % e, -2
+
+
+def GetDescriptionFromId(con,cur,table,cid):
+	"""
+	Get the description for id cid in table
+
+	input:
+	con,cur
+	table : str
+	table : str
+		Name of the table to search
+	cid : int
+		the id to get the description for
+
+	output:
+	err : str
+		Error message or empty string if ok
+	description : str
+		the description of the id
+	"""
+	try:
+		cur.execute('SELECT description FROM %s WHERE id=%s LIMIT 1' % (table,'%s'),[cid])
+		if cur.rowcount==0:
+			debug(2,'id not found in table %s' % table)
+			return 'id not found in table %s' % table,''
+		description=cur.fetchone()[0]
+		debug(1,'found description %s for id %d in table %s' % (description,cid,table))
+		return '',description
+	except psycopg2.DatabaseError as e:
+		debug(8,"error %s in GetDescriptionFromId" % e)
+		return 'Error %s in GetDescriptionFromId' % e, ''
