d83a4e0c1c37c9880eabb529f73bb82880051aaa
amnona <amnonim@gmail.com>
2017-01-19 11:52:40 -0800

    add number of experiments to dbstats

--- newcode/dbstats.py
+++ newcode/dbstats.py
@@ -39,6 +39,6 @@ def GetStats(con, cur):
     explist = set()
     for cres in res:
         explist.add(cres[0])
-    stats['NumOntologyTerms'] = len(explist)
+    stats['NumSequences'] = len(explist)
 
     return '', stats
