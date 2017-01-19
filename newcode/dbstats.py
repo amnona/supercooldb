09555a330e41457881240961be9d83a00c8891c3
amnona <amnonim@gmail.com>
2017-01-19 11:29:40 -0800

    add number of experiments to dbstats

--- newcode/dbstats.py
+++ newcode/dbstats.py
@@ -1,28 +1,44 @@
 from utils import debug
-import psycopg2
 
 
-def GetStats(con,cur):
-	"""
-	Get statistics about the database
-	input:
-	con,cur : database connection and cursor
+def GetStats(con, cur):
+    """
+    Get statistics about the database
+    input:
+    con,cur : database connection and cursor
 
-	output:
-	errmsg : str
-		"" if ok, error msg if error encountered
-	stats : json
-		containing statistics about the database tables
-	"""
+    output:
+    errmsg : str
+        "" if ok, error msg if error encountered
+    stats : json
+        containing statistics about the database tables
+    """
 
-	# number of unique sequences
-	stats={}
-	cur.execute("SELECT reltuples AS approximate_row_count FROM pg_class WHERE relname = 'sequencestable'")
-	stats['NumSequences']=cur.fetchone()[0]
-	cur.execute("SELECT reltuples AS approximate_row_count FROM pg_class WHERE relname = 'annotationstable'")
-	stats['NumAnnotations']=cur.fetchone()[0]
-	cur.execute("SELECT reltuples AS approximate_row_count FROM pg_class WHERE relname = 'sequencesannotationtable'")
-	stats['NumSeqAnnotations']=cur.fetchone()[0]
-	cur.execute("SELECT reltuples AS approximate_row_count FROM pg_class WHERE relname = 'ontologytable'")
-	stats['NumOntologyTerms']=cur.fetchone()[0]
-	return '',stats
+    # number of unique sequences
+    debug(1, 'Get db stats')
+    stats = {}
+    # get number of sequences
+    cur.execute("SELECT reltuples AS approximate_row_count FROM pg_class WHERE relname = 'sequencestable'")
+    stats['NumSequences'] = cur.fetchone()[0]
+
+    # get number of annotations
+    cur.execute("SELECT reltuples AS approximate_row_count FROM pg_class WHERE relname = 'annotationstable'")
+    stats['NumAnnotations'] = cur.fetchone()[0]
+
+    # get number of sequence annotations
+    cur.execute("SELECT reltuples AS approximate_row_count FROM pg_class WHERE relname = 'sequencesannotationtable'")
+    stats['NumSeqAnnotations'] = cur.fetchone()[0]
+
+    # get number of ontologies
+    cur.execute("SELECT reltuples AS approximate_row_count FROM pg_class WHERE relname = 'ontologytable'")
+    stats['NumOntologyTerms'] = cur.fetchone()[0]
+
+    # get number of experiments
+    cur.execute("SELECT expid from experimentsTable")
+    res = cur.fetchall()
+    explist = set()
+    for cres in res:
+        explist.add([cres[0]])
+    stats['NumOntologyTerms'] = len(explist)
+
+    return '', stats
