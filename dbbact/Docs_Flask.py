62af8332e3f224de23ba94264d9145e84ed2f116
amnona <amnonim@gmail.com>
2017-01-26 22:48:16 -0800

    fix autodocs

--- dbbact/Docs_Flask.py
+++ dbbact/Docs_Flask.py
@@ -5,9 +5,10 @@ from .autodoc import auto
 
 Docs_Flask_Obj = Blueprint('Docs_Flask_Obj', __name__, template_folder='templates')
 
-@login_required
+# @login_required
 @Docs_Flask_Obj.route('/docs', methods=['POST', 'GET'])
 def docs():
+	debug(1,g.amnon)
     '''
     The documentation for all the REST API using flask-autodoc
     '''
