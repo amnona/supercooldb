2abdeb52b5d84a942b24b85186c89780a18fb476
amnona <amnonim@gmail.com>
2017-01-26 22:37:34 -0800

    fix autodocs

--- dbbact/Docs_Flask.py
+++ dbbact/Docs_Flask.py
@@ -1,9 +1,11 @@
 from flask import Blueprint
+# from flask.ext.login import current_user
+from flask.ext.login import login_required
 from .autodoc import auto
 
 Docs_Flask_Obj = Blueprint('Docs_Flask_Obj', __name__, template_folder='templates')
 
-
+@login_required
 @Docs_Flask_Obj.route('/docs', methods=['POST', 'GET'])
 def docs():
     '''
