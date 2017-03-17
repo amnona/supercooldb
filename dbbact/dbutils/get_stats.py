ec5da6f02f7a74ecf758cb088f408c68da7f0b69
amnona <amnonim@gmail.com>
2017-03-17 10:43:45 -0700

    add script for db stats

--- dbbact/dbutils/get_stats.py
+++ dbbact/dbutils/get_stats.py
@@ -79,6 +79,8 @@ def get_num_annotations_per_sequence(con, cur, outdir):
         cur2 = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
         cur2.execute('SELECT * FROM SequencesAnnotationTable WHERE seqId=%s', [cseq_data['id']])
         num_annotations = cur2.rowcount
+        if num_annotations > 40:
+            print('%s (%s): %d annotations' % (cseq_data['taxonomy'], cseq_data['sequence', num_annotations]))
         hist[num_annotations] += 1
     with open(path.join(outdir, 'annotations_per_seq.txt'), 'w') as fl:
         fl.write('num_annotations\tnum_sequences\n')
