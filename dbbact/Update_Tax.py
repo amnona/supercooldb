e635eb0dc0efed60542916da2928c3b709566d52
eitanoz <admin@admins-iMac.local>
2017-09-08 19:46:26 +0300

    Add Taxonomy script

---
+++ dbbact/Update_Tax.py
@@ -0,0 +1,148 @@
+from utils import debug, SetDebugLevel
+import db_access
+import dbsequences
+import dbuser
+import os
+import time
+import datetime
+from pathlib import Path
+
+
+def removeFile(file_name):
+    try:
+        os.remove(file_name)
+    except OSError:
+        pass
+
+def createSeqFile(file_name,seq_str):
+    with open(file_name, "w") as text_file:
+        text_file.write(">seq\n%s" % seq_str)
+
+def saveStringToFile(file_name,content_str):
+    with open(file_name, "w") as text_file:
+        text_file.write(content_str)        
+        
+def readResultFromFile(file_name):
+    ret = ""
+    try:
+        with open (file_name, "r") as myfile:
+            ret=myfile.readlines()
+    except:
+        pass
+    
+    return ret
+
+def isFileExist(fileName):
+    my_file = Path("stop")
+    if my_file.is_file():
+        # file exists
+        return True
+    return False
+
+if __name__ == '__main__':
+    SetDebugLevel(0)
+    date_time_str = datetime.datetime.now().strftime("%Y-%m-%d--%H:%M:%S")
+    
+    #remove stop file
+    removeFile("stop")
+    
+    #connect to the db
+    con, cur = db_access.connect_db()
+    
+    debug(2, 'Started')
+    if 'OPENU_FLAG' in os.environ:
+        debug(2, 'Openu')
+    else:
+        debug(2, 'normal')
+    
+    rank_list = []
+    rank_list.append("rootrank")
+    rank_list.append("life")
+    rank_list.append("domain")
+    rank_list.append("kingdom")
+    rank_list.append("phylum")
+    rank_list.append("class")
+    rank_list.append("order")
+    rank_list.append("family")
+    rank_list.append("genus")
+    rank_list.append("species")
+    
+    count_success = 0
+    count_failure = 0
+    count_seq_success = 0
+    count_seq_failure = 0
+    count = 1
+    tax_log = ""
+    rdp_exe_location = "rdp_classifier_2.12/"
+    sleep_time = 10
+        
+    while isFileExist("stop") == False:
+        removeFile("%sinput" %  rdp_exe_location)
+        removeFile("%soutput" %  rdp_exe_location)
+        
+        err, seq_id = dbsequences.GetSequenceWithNoTaxonomyID(con, cur)
+        if err or seq_id == -1:
+            #If no empty sequence, wait for long time
+            time.sleep(sleep_time)
+        
+        tax_log += "sequence id = " + str(seq_id) + "\n"
+        
+        err, seq_str = dbsequences.GetSequenceStrByID(con, cur, seq_id)
+        if err:
+            tax_log += "Fatal Error, could not find sequence " + "\n"
+            break
+        
+        
+        #java -Xmx1g -jar dist/classifier.jar classify  -o output_filename example.fasta
+        input_file_name = "%sinput" %  rdp_exe_location
+        output_file_name = "%soutput" %  rdp_exe_location
+        
+        #get the taxononmy for specific sequence
+        createSeqFile(input_file_name,seq_str)
+        os.system("java -Xmx1g -jar %sdist/classifier.jar classify  -o %s %s" % (rdp_exe_location,output_file_name,input_file_name))
+        tex_res = readResultFromFile(output_file_name)
+        
+        tax_log += "the data:\n"
+        for line in tex_res:
+            tax_log += line + "\n"
+            data = line.split('\t')
+            
+        
+        #search for the string
+        prev = ""
+        has_failure = False
+        
+        for x in data:
+            has_failure = False
+            for y in rank_list:
+                if x == y:
+                    # Add to DB
+                    tax_log += x + " = " +  prev 
+                    if dbsequences.AddSequenceTax(con, cur, seq_id, "tax" + x, prev) == True:
+                        tax_log += " SUCCESS" + "\n"
+                        count_seq_success = count_seq_success + 1
+                    else:
+                        tax_log += " FAILED" + "\n"
+                        count_seq_failure = count_seq_failure + 1
+                        has_failure = True
+            # keep the previous value    
+            prev = x
+            prev = prev.replace("\"", "")
+        
+        if has_failure == True:
+            count_failure = count_failure + 1
+        else:
+            count_success = count_success + 1
+        
+        
+        summary_str = "count_success = %s\ncount_failure = %s\ncount_seq_success = %s\ncount_seq_failure = %s\n" % (count_success,count_failure,count_seq_success,count_seq_failure)
+        
+        saveStringToFile("tax_summary_log_" + date_time_str,summary_str)
+        saveStringToFile("tax_log_" + date_time_str,tax_log)
+        debug(2, 'found sequence %s' % seq_str)
+        debug(2, 'return %s' % tex_res)
+        count = count + 1
+        
+        #stop the script in case of error
+        if count_failure > 0:
+            break;
