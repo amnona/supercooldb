f08de21bd2214653f48aae91a1295add24c45115
amnona <amnonim@gmail.com>
2017-12-17 00:32:14 +0200

    handle access control forin browser request

--- dbbact/Seq_Flask.py
+++ dbbact/Seq_Flask.py
@@ -6,7 +6,7 @@ import dbannotations
 import dbontology
 from utils import debug, getdoc
 from autodoc import auto
-from flask_cors import crossdomain
+from flask_cors import crossdomain, cross_origin
 # from flask_cors import CORS
 
 Seq_Flask_Obj = Blueprint('Seq_Flask_Obj', __name__, template_folder='templates')
@@ -548,7 +548,8 @@ def get_sequence_info():
 
 @login_required
 @Seq_Flask_Obj.route('/sequences/get_string_annotations', methods=['GET', 'POST', 'OPTIONS'])
-@crossdomain(origin='*')
+@cross_origin(allow_headers=['Content-Type'], origin='*')
+# @crossdomain(origin='*')
 @auto.doc()
 def get_sequence_string_annotations():
     """
